d8ea9de92b319c02728bba5d34fa2862c869789b
Goose <jrpedroza99@gmail.com>
2023-11-02 13:57:24 -0700

    updated server and client files

--- client.py
+++ client.py
@@ -1,1 +1,40 @@
 # Client
+
+import socket
+import sys
+import threading
+
+def send_message(client_socket):
+    while True:
+        message = input()
+        client_socket.send(message.encode())
+
+def receive_messages(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode()
+            print(message)
+        except:
+            print("Connection closed.")
+            client_socket.close()
+            sys.exit()
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Prompt the user for server IP and port
+server_ip = input("Enter the server IP address: ")
+server_port = int(input("Enter the server port: "))
+
+server_address = (server_ip, server_port)
+client_socket.connect(server_address)
+
+# Prompt the user to enter their name
+user = input("Enter your name: ")
+client_socket.send(user.encode())
+
+# Create two threads for sending and receiving messages
+send_thread = threading.Thread(target=send_message, args=(client_socket,))
+receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+
+send_thread.start()
+receive_thread.start()
--- server.py
+++ server.py
@@ -1,1 +1,54 @@
-# Server file
+# Server
+
+import socket
+import select
+
+def receive_message(client_socket):
+    try:
+        message = client_socket.recv(1024).decode()
+        if not message:
+            return False
+        return message
+    except:
+        return False
+
+# Prompt the user for server address and port
+server_ip = input("Enter the server IP address: ")
+server_port = int(input("Enter the server port: "))
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Define server address and port based on user input
+server_address = (server_ip, server_port)
+server_socket.bind(server_address)
+server_socket.listen(5)
+
+sockets_list = [server_socket]
+clients = {}
+
+print("Server is running on {}:{}".format(server_ip, server_port))
+
+while True:
+    read_sockets, _, _ = select.select(sockets_list, [], [])
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
+            user = receive_message(client_socket)
+            if user is False:
+                continue
+            sockets_list.append(client_socket)
+            clients[client_socket] = user
+            print("Accepted connection from {}:{}".format(client_address[0], client_address[1]))
+        else:
+            message = receive_message(notified_socket)
+            if message is False:
+                print("Closed connection from {}".format(clients[notified_socket]))
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+                continue
+            user = clients[notified_socket]
+            print(f"Received message from {user}: {message}")
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(f"{user}: {message}".encode())
